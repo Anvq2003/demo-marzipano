2d47780c6158965c7bc69cd51a53be50a0966e6c
an.voquoc <an.voquoc@alta.com.vn>
2024-02-19 18:30:01 +0700

    demo

---
+++ src/app/[[...slug]]/interfaces.ts
@@ -0,0 +1,20 @@
+export interface ISettingsPano {
+  mouseViewMode: string;
+  autorotateEnabled?: boolean;
+  fullscreenButton?: boolean;
+  viewControlButtons?: boolean;
+}
+
+export interface SceneRef {
+  sceneData: Scene;
+  scene: any; 
+}
+
+export interface Scene {
+  id: string;
+  name: string;
+  levels: { tileSize: number; size: number; fallbackOnly?: boolean }[];
+  faceSize: number;
+  initialViewParameters: { pitch: number; yaw: number; fov: number };
+  linkHotspots: { yaw: number; pitch: number; target: string }[];
+}
